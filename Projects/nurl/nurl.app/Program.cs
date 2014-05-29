e6f27f80892860676938aa3508a424026a9afc5c
Joey Bronner <joeybronner@gmail.com>
2014-05-29 15:52:46 +0200

    architecture (folders, docs & two distinct projects) Signed-off-by: Joey Bronner <joeybronner@gmail.com>

---
+++ Projects/nurl/nurl.app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nurl.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("-NURL PROJECT- https://github.com/joeybronner/CSharpProjectsExercices.git \n");
+			Console.WriteLine("Starting...\n");
+			
+			// calling the start method (file: ...)
+			
+			Console.Read();
+        }
+    }
+}
